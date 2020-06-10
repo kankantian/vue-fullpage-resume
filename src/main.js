8f0c879523c37e6f1bbf687882ee2ec15b4fd682
kankantian <306423466@qq.com>
2020-06-11 00:39:14 +0800

    引入fullpage

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "../src/static/css/fullpage.min.css"
+import VueFullPage from 'vue-fullpage.js'
+
+Vue.use(VueFullPage);
+
 Vue.config.productionTip = false
 
 new Vue({
